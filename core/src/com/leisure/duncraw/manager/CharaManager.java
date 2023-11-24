f177e6195ad3299b3e4318f73cc8ca9d18df7ad6
ark <divineswordbearer@gmail.com>
2023-11-24 21:05:16 +0800

    Removed some silly embed password, huhu

--- core/src/com/leisure/duncraw/manager/CharaManager.java
+++ core/src/com/leisure/duncraw/manager/CharaManager.java
@@ -1,8 +1,18 @@
 package com.leisure.duncraw.manager;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.leisure.duncraw.art.chara.Chara;
 import com.leisure.duncraw.data.CharasData;
 
 public class CharaManager {
-  public CharaManager(CharasData sources) {}
+  public ArrayList<Chara> charas = new ArrayList<>();
+  public CharaManager(CharasData sources) { 
+  }
+  public void updateAll() {
+  }
+  public void renderAll() {
+
+  }
 }
