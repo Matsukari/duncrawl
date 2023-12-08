5807c5aa099482aaaea93e4d82473d7e27fe5bd2
ark <divineswordbearer@gmail.com>
2023-12-08 14:33:56 +0800

    Room generation (rectangles) sub rooms test

--- core/src/com/leisure/duncraw/map/generator/RoomsBuilder.java
+++ core/src/com/leisure/duncraw/map/generator/RoomsBuilder.java
@@ -14,7 +14,6 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.FloatArray;
-import com.leisure.duncraw.logging.Logger;
 
 import lib.math.Circle;
 import lib.math.DelaunayTriangulator;
@@ -25,6 +24,7 @@ public class RoomsBuilder {
   public ArrayList<Edge> corridors = new ArrayList<>();
   public ArrayList<Rectangle> rooms = new ArrayList<>();
   public ArrayList<Rectangle> mainRooms = new ArrayList<>();
+  public ArrayList<Rectangle> subRooms = new ArrayList<>();
   public HashMap<Rectangle, ArrayList<Rectangle>> roomConnections = new HashMap<>();
   public Vector2 roomsCenter = Vector2.Zero;
   public float mainRoomMinSize = 200;
@@ -32,34 +32,50 @@ public class RoomsBuilder {
 
   public void build(int roomsNum, Vector2 maxSize, Vector2 widthRange, Vector2 heightRange) {
     this.roomsNum = roomsNum;
-    genRandomRooms(maxSize, widthRange, heightRange);
-    separateRooms(maxSize.x);
-    connectRooms();
-
-    createPaths();
-    createCorridors();
-  }
-  private void genRandomRooms(Vector2 maxSize, Vector2 widthRange, Vector2 heightRange) {
+    mainRooms = genRandomRooms(roomsNum, maxSize, widthRange, heightRange);
+    subRooms = genRandomRooms(roomsNum * 10, maxSize.sub(maxSize.x/3, maxSize.y/3), widthRange, heightRange);
     rooms.clear();
-    mainRooms.clear();
+    rooms.addAll(mainRooms);
+    separateRooms(mainRooms, roomsCenter, maxSize.x);
+    separateRooms(subRooms, roomsCenter, maxSize.x);
+    nodes = connectRooms(mainRooms);
+
+    roomConnections = createPaths(nodes);
+    corridors = createCorridors(roomConnections);
+    
+    ArrayList<Rectangle> trimmedSubRooms = new ArrayList<>();
+    // Trim; delete rooms that aren't connected by corridors; ie., rooms that are unreachable
+    for (Rectangle subRoom : subRooms) { for (Edge corridor : corridors) {
+      if (Intersector.intersectSegmentRectangle(corridor.p1, corridor.p2, subRoom)) {
+        trimmedSubRooms.add(subRoom);
+        break;
+      }
+    } }
+    subRooms = trimmedSubRooms;
+    rooms.addAll(subRooms);
+    // Logger.log("RoomsBuilder", String.format("Rooms %d left", rooms.size()));
+  }
+  // Main rooms scattered
+  private ArrayList<Rectangle> genRandomRooms(int roomsNum, Vector2 maxSize, Vector2 widthRange, Vector2 heightRange) {
+    ArrayList<Rectangle> newRooms = new ArrayList<>();
     Circle circle = new Circle(0, 0, 10);
     for (int it = 0; it < roomsNum; it ++) {
       Vector2 pos = circle.getRandomPoint();
       Rectangle rect = new Rectangle(pos.x, pos.y, 
         MathUtils.random(widthRange.x, widthRange.y) * maxSize.x, 
         MathUtils.random(heightRange.x, heightRange.y) * maxSize.y);
-      rooms.add(rect);     
-      if (rect.width + rect.height >= mainRoomMinSize) mainRooms.add(rect);
+      newRooms.add(rect);
     }
+    return newRooms;
   }
-  private void separateRooms(float boundary) {
+  private void separateRooms(ArrayList<Rectangle> roomsDiv, Vector2 center, float boundary) {
     // Ensure no rooms are within the roomSize
-    for (int it = 0; it < boundary*rooms.size(); it++) {
-      for (int i = 0; i < rooms.size(); i++) {
-        for (int j = 0; j < rooms.size(); j++) {
-          Rectangle a = rooms.get(i), b = rooms.get(j), inters = new Rectangle();
+    for (int it = 0; it < boundary*roomsDiv.size(); it++) {
+      for (int i = 0; i < roomsDiv.size(); i++) {
+        for (int j = 0; j < roomsDiv.size(); j++) {
+          Rectangle a = roomsDiv.get(i), b = roomsDiv.get(j), inters = new Rectangle();
           if (j != i && Intersector.intersectRectangles(a, b, inters)) {
-            float angle = MathUtils.atan2(roomsCenter.y - b.y, roomsCenter.x - b.x) * 180f / MathUtils.PI;
+            float angle = MathUtils.atan2(center.y - b.y, center.x - b.x) * 180f / MathUtils.PI;
             b.x += MathUtils.cosDeg(angle) * inters.width; 
             b.y += MathUtils.sinDeg(angle) * inters.height;
             a.x -= MathUtils.cosDeg(angle) * inters.width;
@@ -69,17 +85,17 @@ public class RoomsBuilder {
       }
     }
   }
-  private void connectRooms() {
+  private ArrayList<Edge> connectRooms(ArrayList<Rectangle> roomsDiv) {
     FloatArray points = new FloatArray();
-    for (Rectangle rect : rooms) {
+    for (Rectangle rect : roomsDiv) {
       points.add((float)rect.x + rect.width/2);
       points.add((float)rect.y + rect.height/2);
     }
-    nodes = new DelaunayTriangulator(points).getEdges(); 
+    return new DelaunayTriangulator(points).getEdges(); 
   }
-  public void createCorridors() {
-    corridors.clear(); 
-    for (Map.Entry<Rectangle, ArrayList<Rectangle>> room : roomConnections.entrySet()) {
+  public ArrayList<Edge> createCorridors(HashMap<Rectangle, ArrayList<Rectangle>> rConnections) {
+    ArrayList<Edge> newCorridors = new ArrayList<>(); 
+    for (Map.Entry<Rectangle, ArrayList<Rectangle>> room : rConnections.entrySet()) {
       // Logger.log("createCorridors", "Creating a corridor...");
       Rectangle a = room.getKey();
       float aX = a.x + a.width/2;
@@ -98,11 +114,7 @@ public class RoomsBuilder {
           lineA.p2.x = aX;
           lineA.p2.y = bY;
           horizontal = false;
-          // Logger.log("First line", "Created vertical line");
         }
-        // else 
-        //   Logger.log("Intersector", "Created horizontal line");
-        // Rectangle intersection = new Rectangle();
         // Doesn't overlap at one axis, then create another line going from initial line to destination room; guranteed intersection
         if (!Intersector.intersectSegmentRectangle(lineA.p1, lineA.p2, b)) {
           // Preivous default line was horizontal then now; vertical
@@ -111,39 +123,38 @@ public class RoomsBuilder {
           if (!horizontal) {
             lineB.p2.x = bX;
             lineB.p2.y = lineA.p2.y;
-            // Logger.log("Intersector", "Created another horizontal line");
           }
-          // else
-          //   Logger.log("Intersector", "Created another vertical line");
         }
-        corridors.add(lineA);
-        if (lineB != null) corridors.add(lineB);
+        newCorridors.add(lineA);
+        if (lineB != null) newCorridors.add(lineB);
       } 
     }
-    Logger.log("Created corridors", Integer.toString(corridors.size()));
+    return newCorridors;
+    // Logger.log("Created corridors", Integer.toString(corridors.size()));
   }
-  private void createPaths() {
-    roomConnections.clear();
+  private HashMap<Rectangle, ArrayList<Rectangle>> createPaths(ArrayList<Edge> paths) {
+    HashMap<Rectangle, ArrayList<Rectangle>> rConnections = new HashMap<>();
     Graph<Vector2, DefaultWeightedEdge> graph = new SimpleGraph<>(DefaultWeightedEdge.class);
-    for (Edge edge : nodes) {
+    for (Edge edge : paths) {
       graph.addVertex(edge.p1);
       graph.addVertex(edge.p2);
       graph.addEdge(edge.p1, edge.p2);
       Rectangle a = getRoomPointSrc(edge.p1);
       Rectangle b = getRoomPointSrc(edge.p2);
-      ArrayList<Rectangle> connections = roomConnections.get(a);
+      ArrayList<Rectangle> connections = rConnections.get(a);
       
       if (connections == null) connections = new ArrayList<>();
       connections.add(b);
 
-      roomConnections.put(a, connections);
+      rConnections.put(a, connections);
     }
     // Logger.log("Minimum", "Created " + Integer.toString(roomConnections.size()));
-    nodes.clear();
+    paths.clear();
     PrimMinimumSpanningTree<Vector2, DefaultWeightedEdge> prim = new PrimMinimumSpanningTree<>(graph);
     for (DefaultWeightedEdge edge : prim.getSpanningTree().getEdges()) {
-      nodes.add(new Edge(graph.getEdgeSource(edge), graph.getEdgeTarget(edge)));
+      paths.add(new Edge(graph.getEdgeSource(edge), graph.getEdgeTarget(edge)));
     }
+    return rConnections;
   }
   // on the basis that each rectangles does not overlap with any other
   public Rectangle getRoomPointSrc(Vector2 point) {
