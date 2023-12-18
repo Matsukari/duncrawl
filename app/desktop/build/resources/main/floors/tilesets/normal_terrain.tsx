f1154e1788c6d27771a0c2cc62aa91a126d470b4
ark <divineswordbearer@gmail.com>
2023-12-18 15:51:36 +0800

    Combined platform codes under a single folder

---
+++ app/desktop/build/resources/main/floors/tilesets/normal_terrain.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="normal_terrain" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <image source="../../images/terrains/semi_surface.png" width="320" height="320"/>
+ <tile id="43">
+  <properties>
+   <property name="terrain" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="terrain" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="123">
+  <properties>
+   <property name="terrain" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="terrain" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="terrain" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="terrain" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="181">
+  <properties>
+   <property name="terrain" value="ground"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="terrain" value="ground"/>
+  </properties>
+ </tile>
+</tileset>
