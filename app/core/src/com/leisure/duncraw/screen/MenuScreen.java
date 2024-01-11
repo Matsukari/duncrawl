18f6d03412894782984eb1d4e0b4aa56861b62da
ark <divineswordbearer@gmail.com>
2024-01-11 10:26:04 +0800

    Added main menu holder (only taking play game)

--- app/core/src/com/leisure/duncraw/screen/MenuScreen.java
+++ app/core/src/com/leisure/duncraw/screen/MenuScreen.java
@@ -2,13 +2,15 @@ package com.leisure.duncraw.screen;
 
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.leisure.duncraw.data.AssetSource;
 import com.leisure.duncraw.data.SaveData;
 import com.leisure.duncraw.logging.Logger;
 
 public class MenuScreen extends Screen {
-  private SaveData save;
+  protected SaveData save;
   public MenuScreen() {
     Logger.log("MenuScreen", "Init");
     change();
--- app/desktop/src/com/leisure/duncraw/MenuScreenInput.java
+++ app/desktop/src/com/leisure/duncraw/MenuScreenInput.java
@@ -1,13 +1,47 @@
 package com.leisure.duncraw;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.leisure.duncraw.data.AssetSource;
+import com.leisure.duncraw.hud.Hud;
 import com.leisure.duncraw.screen.MenuScreen;
 import com.leisure.duncraw.screen.Screen;
 
 public class MenuScreenInput extends MenuScreen implements InputProcessor {
+  public Stage stage;
+  public Table root;
+  public Label options[] = new Label[3]; 
   public MenuScreenInput() {
-    change();
+    stage = new Stage();
+    root = new Table();
+    root.setFillParent(true);
+    options[0] = Hud.createLabel("Play game");
+    options[0].addListener(new ClickListener() {
+      @Override
+      public void clicked(InputEvent event, float x, float y) {
+        change();
+      }
+    });
+    options[1] = Hud.createLabel("Options");
+    options[2] = Hud.createLabel("Credits");
+    for (Label option : options) {
+      root.add(option).center().expand();
+      root.row();
+    }
+    
+    stage.addActor(root);
+  }
+  @Override
+  public void render(float delta) {
+    ScreenUtils.clear(Color.BLACK);
+    stage.act();
+    stage.draw();
   }
   @Override
   public Screen next() {
